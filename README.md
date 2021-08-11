1fc331325a5bdd0e63d27b4054faf7ecc32f38d0
Armon Raphiel <araphiel@gmail.com>
2021-08-11 16:16:34 -0500

    init commits

---
+++ README.md
@@ -0,0 +1,14 @@
+# Tasks
+
+1. Fetch post data from the specified API.
+
+2. Use DOM APIs to render that post data on the page.
+
+3. (Optional) Add some basic styling to make it look nice.
+
+- Use the native DOM APIs and standard (ES5/ES6) javascript.
+- Feel free to search for any documentation as you normally would.
+
+- *Hints & things that may help*
+    - Template literals
+    - element.insertAdjacentHTML
---
+++ src/main.js
@@ -0,0 +1,15 @@
+// Fetch post data
+async function fetchPosts() {
+    const endpoint = `https://jsonplaceholder.typicode.com/posts`
+    return posts
+}
+
+// On page load
+document.addEventListener('DOMContentLoaded', () => {
+    // The parent element where content should be loaded into
+    const app = document.getElementById('app')
+
+    // Remove this line
+    setTimeout(() => alert('edit the javascript in the main.js file'), 1000)
+
+})
